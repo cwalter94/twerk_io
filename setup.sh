b09e4fc5198f90a7af01c6bcba49582146a970b0
Chris <cwalter@ledaenergy.com>
2015-02-14 18:00:24 +0100

    added setup command

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-MONGODB='104.131.155.162:23000/twerk' JWT_SECRET='something something something KANYE WEST BEST RAPPER ALIVE' ADMIN_PW='1834CA1896webmaster' PORT=80 CDN=1 forever start app.js
+MONGODB='104.131.155.162:23000/twerk' JWT_SECRET='somethingaksjdfoiejaflskjdfaoisefasldkfjE' ADMIN_PW='1834CA1896webmaster' PORT=80 CDN=1 forever start app.js
